1c35dd5ea5ae1c8e9ec967f1a0a895c9d20b14f9
agudat <n0527677043@gmail.com>
2026-08-09 00:22:45 +0300

    Deploy missing-fields warning on factory/supervisor edit forms (build bmskvks1r)
    Pruned 2 oldest build directories (bmrxxzs2y, bmry0bngn).

--- assets/bmry0bngn/use-debounced-value-CzTQKJz5.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./vendor-table-54F5gVDw.js";function s(e,t=350){const[r,u]=o.useState(e);return o.useEffect(()=>{const c=setTimeout(()=>u(e),t);return()=>clearTimeout(c)},[e,t]),r}export{s as u};
--- assets/bmry0bngn/use-permission-D7B5qFAk.js
+++
@@ -1,1 +0,0 @@
-import{b as t,U as r}from"./index-CXlgwLSm.js";function c(){const{role:a}=t(),e=a===r.Admin,n=a===r.Secretary;return{canCreate:e||n,canEdit:e||n,canArchive:e||n,canApprovePay:e||n,canManageSettings:e,canDelete:e}}export{c as u};
